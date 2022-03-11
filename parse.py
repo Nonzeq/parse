d1e50da8350c7567731102dd6806c9c83abae010
bogdan <bgdn.kobylchak@gmail.com>
2022-03-11 20:31:39 +0200

    test

---
+++ parse.py
@@ -0,0 +1,29 @@
+#!/home/bogdan/parse/venv/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def get_html():
+    url = 'https://bank.gov.ua/ua/markets/exchangerates'
+    r = requests.get(url)
+    return r.text
+
+
+def get_dollar(html):
+    soup = BeautifulSoup(html, 'lxml')
+    t = soup.find('td', text='USD').find_parent('tr').find_all('td')[-1].text
+    return t
+
+def message(msg):
+    title = 'Долар США'
+    os.system('notify-send "{}" "{}"'.format(title, msg))
+
+
+def main():
+    rate = get_dollar(get_html())
+    message(rate)
+
+
+if __name__ == '__main__':
+    main()
